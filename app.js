e172eb89e5a6b162228918f90c0930b4ee315dea
teppei1204 <m-n-m09@docomo.ne.jp>
2025-03-24 00:43:17 +0900

    動画投稿処理の実装

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3002
 
+app.use(express.json())//jsonを使えるようにするよという記述
+
+// ここからPOSTMANにて実践
+// 本来ならデータベースに保存するが今回は省略
+app.post('/booklog',(req,res) => {
+  const booklog =  req.body
+
+  // if (!true)の意味は本来とは逆転してfalseという意味になる
+  if (!(booklog.name && booklog.text)) {
+    return res.json({
+      "ok": false,
+      "error": "invalid parameter"
+    })
+  }
+
+    res.json({
+      "ok":true,
+      "booklog": booklog
+    })
+  })
+
 app.listen(port,() => {//この第二引数は無名関数という
   console.log('app listening at http://localhost:${port}');
 })
+// 機能(コード)を追記したらサーバー再起動
