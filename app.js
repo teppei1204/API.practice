d29f338938c3bd7f1c0862a4f6bd6299683bc559
teppei1204 <m-n-m09@docomo.ne.jp>
2025-03-24 00:51:49 +0900

    動画一覧取得処理

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3002
+let booklog = {}//本当は複数形が望ましいが、複雑になるので単数形で処理する
 
 app.use(express.json())//jsonを使えるようにするよという記述
 
 // ここからPOSTMANにて実践
 // 本来ならデータベースに保存するが今回は省略
 app.post('/booklog',(req,res) => {
-  const booklog =  req.body
+  booklog =  req.body
 
   // if (!true)の意味は本来とは逆転してfalseという意味になる
   if (!(booklog.name && booklog.text)) {
@@ -22,6 +23,15 @@ app.post('/booklog',(req,res) => {
       "booklog": booklog
     })
   })
+
+app.get("/booklog", (req, res) => {
+  res.json({
+    "ok": true,
+    "booklog": [//一覧なので配列
+      booklog
+    ]
+  })
+})
 
 app.listen(port,() => {//この第二引数は無名関数という
   console.log('app listening at http://localhost:${port}');
